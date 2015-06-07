ec1f1c0fc0315cba3f18c131e688a1cc13e47606
Markus Kainu <markuskainu@gmail.com>
2015-06-07 15:18:11 +0200

    shinydashboard version

--- server.R
+++ server.R
@@ -1,9 +1,14 @@
+library(shiny)
+library(shinydashboard)
 library(dplyr)
 library(tidyr)
 library(ggplot2)
 library(scales)
 library(stringr)
 library(lubridate)
+library(tm)
+library(wordcloud)
+
 
 col24 <-  c("#89C5DA", "#DA5724", "#74D944", "#CE50CA", "#3F4921", "#C0717C", "#CBD588", "#5F7FC7", 
             "#673770", "#D3D93E", "#38333E", "#508578", "#D7C1B1", "#689030", "#AD6F3B", "#CD9BCD", 
@@ -13,10 +18,14 @@ col24 <-  c("#89C5DA", "#DA5724", "#74D944", "#CE50CA", "#3F4921", "#C0717C", "#
             "#D14285", "#6DDE88", "#652926", "#7FDCC0", "#C84248", "#8569D5", "#5E738F", "#D1A33D", 
             "#8A7C64", "#599861")
 
-
-
-shinyServer(function(input, output, session) {
+function(input, output) {
+  set.seed(122)
+  histdata <- rnorm(500)
   
+  output$plot1 <- renderPlot({
+    data <- histdata[seq_len(input$slider)]
+    hist(data)
+  })
   
   output$file_input <- renderUI({
     
@@ -61,8 +70,24 @@ shinyServer(function(input, output, session) {
     dat
   })
   
-  ### --------------------------------------------------------------------- ###
-  # -- Which domain within group
+  output$time_frame <- renderUI({
+    
+    dat <- data_import()
+    opts <- dateRangeInput("timeFrame", "Date range:",
+                           start = min(dat$date),
+                           end   = max(dat$date))
+    list(opts)
+  })
+  
+  output$mytable <- renderDataTable({
+    
+    dat <- data_import()
+    # subset time
+    dat <- dat[dat$date >= input$timeFrame[1] & dat$date <= input$timeFrame[2],]
+    # subset cols
+    dat[c(input$cols)]
+    
+  },options = list(pageLength = 10))
   
   output$select_cols <- renderUI({
     
@@ -74,158 +99,178 @@ shinyServer(function(input, output, session) {
     list(opts)
   })
   
-  output$activity_list <- renderUI({
+  
+  
+  
+  
+  output$activity_list_monthly <- renderUI({
     
     dat <- data_import()
+    dat <- dat[dat$date >= input$timeFrame[1] & dat$date <= input$timeFrame[2],]
     activity_freq <- dat %>% group_by(activity) %>% dplyr::summarise(count = n())
     activities <- as.character(arrange(activity_freq, -count)$activity)
-    opts <- selectInput("activity", "Pick activities:", 
+    opts <- selectInput("activity_monthly", "Pick activities:", 
                         selectize = TRUE, multiple = TRUE,
-                        choices = activities, selected= c(activities[1],activities[2]))
+                        choices = activities, selected= c(activities[1]))
     list(opts)
   })
   
-  output$time_frame <- renderUI({
-
+  output$activity_list_weekly <- renderUI({
+    
     dat <- data_import()
-    opts <- dateRangeInput("timeFrame", "Date range:",
-                   start = min(dat$date),
-                   end   = max(dat$date))
+    dat <- dat[dat$date >= input$timeFrame[1] & dat$date <= input$timeFrame[2],]
+    activity_freq <- dat %>% group_by(activity) %>% dplyr::summarise(count = n())
+    activities <- as.character(arrange(activity_freq, -count)$activity)
+    opts <- selectInput("activity_weekly", "Pick activities:", 
+                        selectize = TRUE, multiple = TRUE,
+                        choices = activities, selected= c(activities[1]))
     list(opts)
   })
   
   
-    output$mytable= renderDataTable({
-      
-      dat <- data_import()
-      # subset time
-      dat <- dat[dat$date >= input$timeFrame[1] & dat$date <= input$timeFrame[2],]
-      # subset activities
-      dat <- dat[dat$activity %in% input$activity,]
-      # subset cols
-      dat[c(input$cols)]
-      
-    },options = list(pageLength = 10))
+  ## Outputs
+  
+  
+  output$pace_point_monthly <- renderPlot({
     
-    output$scatter <- renderPlot({
-      
-      dat <- data_import()
-      dat <- dat[dat$date >= input$timeFrame[1] & dat$date <= input$timeFrame[2],]
-      dat <- dat[dat$activity %in% input$activity,]
-      
-      ggplot(dat, aes(x=time,y=distance.km.,color=month,group=1)) +
-        geom_point(size=3) + 
-        facet_grid(year~activity) +
-        geom_smooth(method="loess") +
-        labs(x="Exercise duration",y="Exercise distance") +
-        theme(axis.text.x = element_text(angle = 45)) +
-        scale_color_manual(values=c("#0868ac","#7bccc4","#feb24c","#fd8d3c","#f03b20","#bd0026",
-                                    "#bd0026","#f03b20","#fd8d3c","#feb24c","#7bccc4","#0868ac")) +
-        theme_bw()
-      
-    })
+    dat <- data_import()
+    dat <- dat[dat$date >= input$timeFrame[1] & dat$date <= input$timeFrame[2],]
+    dat <- dat[dat$activity %in% input$activity_monthly,]
     
+    dat$pace <- dat$time / dat$distance.km.         
+    df <- dat %>% group_by(year,month_num,month,activity) %>% dplyr::summarise(mean_pace = mean(pace))
+    df <- df[!is.na(df$mean_pace),]
+    df$date <- paste(df$year,df$month_num,"01",sep="-")
+    df$date <- as.Date(df$date)
+    if (input$paceTypeMonthly == "km/h" ) df$mean_pace <- 60 / df$mean_pace
+    ylab <- "pace min/km"
+    if (input$paceTypeMonthly == "km/h" ) ylab <- "speed in km/h"
     
-    output$pace_bar <- renderPlot({
-      
-      dat <- data_import()
-      dat <- dat[dat$date >= input$timeFrame[1] & dat$date <= input$timeFrame[2],]
-      dat <- dat[dat$activity %in% input$activity,]
-      
-        dat$pace <- dat$time / dat$distance.km.         
-        df <- dat %>% group_by(year,month_num,month,activity) %>% dplyr::summarise(mean_pace = mean(pace))
-        df <- df[!is.na(df$mean_pace),]
-        df$date <- paste(df$year,df$month_num,"01",sep="-")
-        df$date <- as.Date(df$date)
-        if (input$paceType == "km/h" ) df$mean_pace <- 60 / df$mean_pace
-        ylab <- "pace min/km"
-        if (input$paceType == "km/h" ) ylab <- "speed in km/h"
-        
-      
-      
-      
-      ggplot(df, aes(x=date,y=mean_pace,color=month,group=activity)) +
+    ggplot(df, aes(x=date,y=mean_pace,color=month,group=activity)) +
         geom_point(size=4) +
-        facet_grid(~activity) +
+        facet_grid(~activity, scale="free") +
         geom_smooth(method="loess") +
         labs(x="date",y=ylab) +
         scale_color_manual(values=c("#0868ac","#7bccc4","#feb24c","#fd8d3c","#f03b20","#bd0026",
                                     "#bd0026","#f03b20","#fd8d3c","#feb24c","#7bccc4","#0868ac")) +
         theme_bw()
-      
-    })
     
-    output$minutes_bar_monthly <- renderPlot({
-      
-      dat <- data_import()
-      dat <- dat[dat$date >= input$timeFrame[1] & dat$date <= input$timeFrame[2],]
-      
-      
-      df <- dat %>% group_by(year,month_num,month,activity) %>% dplyr::summarise(sum_time = sum(time))
-      df <- df[!is.na(df$sum_time),]
-      df$date <- paste(df$year,df$month_num,"01",sep="-")
-      df$date <- as.Date(df$date)
-      
-      ggplot(df, aes(x=date,y=sum_time,fill=activity)) +
-        geom_bar(stat="identity", position="stack") +
-        theme_bw() +
-        scale_fill_manual(values = col24)
-      
-    })
+  })
+  
+  output$pace_point_weekly <- renderPlot({
     
-    output$distance_bar_weekly <- renderPlot({
-      
-      dat <- data_import()
-      dat <- dat[dat$date >= input$timeFrame[1] & dat$date <= input$timeFrame[2],]
-      
-      
-      df <- dat %>% group_by(year,week,activity) %>% dplyr::summarise(sum_distance = sum(distance.km.))
-      df <- df[!is.na(df$sum_distance),]
-      
-      ggplot(df, aes(x=week,y=sum_distance,fill=activity)) +
-        geom_bar(stat="identity", position="stack") +
-        theme_bw() +
-        facet_grid(year~.) +
-        scale_fill_manual(values = col24)
-      
-    })
+    dat <- data_import()
+    dat <- dat[dat$date >= input$timeFrame[1] & dat$date <= input$timeFrame[2],]
+    dat <- dat[dat$activity %in% input$activity_weekly,]
     
-    output$minutes_bar_weekly <- renderPlot({
-      
-      dat <- data_import()
-      dat <- dat[dat$date >= input$timeFrame[1] & dat$date <= input$timeFrame[2],]
-      
-      
-      df <- dat %>% group_by(year,week,activity) %>% dplyr::summarise(sum_time = sum(time))
-      df <- df[!is.na(df$sum_time),]
-
-      ggplot(df, aes(x=week,y=sum_time,fill=activity)) +
-        geom_bar(stat="identity", position="stack") +
-        theme_bw() +
-        facet_grid(year~.) +
-        scale_fill_manual(values = col24)
-      
-    })
+    dat$pace <- dat$time / dat$distance.km.         
+    df <- dat %>% group_by(year,week,activity) %>% dplyr::summarise(mean_pace = mean(pace))
+    df <- df[!is.na(df$mean_pace),]
+    if (input$paceTypeWeekly == "km/h" ) df$mean_pace <- 60 / df$mean_pace
+    ylab <- "pace min/km"
+    if (input$paceTypeWeekly == "km/h" ) ylab <- "speed in km/h"
     
-    output$distance_bar_monthly <- renderPlot({
-      
-      dat <- data_import()
-      dat <- dat[dat$date >= input$timeFrame[1] & dat$date <= input$timeFrame[2],]
-      
-      
-      df <- dat %>% group_by(year,month_num,month,activity) %>% dplyr::summarise(sum_distance = sum(distance.km.))
-      df <- df[!is.na(df$sum_distance),]
-      df$date <- paste(df$year,df$month_num,"01",sep="-")
-      df$date <- as.Date(df$date)
-      
-      ggplot(df, aes(x=date,y=sum_distance,fill=activity)) +
-        geom_bar(stat="identity", position="stack") +
-        theme_bw() +
-        scale_fill_manual(values = col24)
-      
-    })
+    ggplot(df, aes(x=week,y=mean_pace,group=activity)) +
+        geom_point(size=4) +
+        facet_grid(year~activity, scales = "free") +
+        geom_smooth(method="loess") +
+        labs(x="date",y=ylab) +
+        scale_color_manual(values=c("#0868ac","#7bccc4","#feb24c","#fd8d3c","#f03b20","#bd0026",
+                                    "#bd0026","#f03b20","#fd8d3c","#feb24c","#7bccc4","#0868ac")) +
+        theme_bw()
     
-})
-
-
-
+  })
+  
+  output$minutes_bar_monthly <- renderPlot({
+    
+    dat <- data_import()
+    dat <- dat[dat$date >= input$timeFrame[1] & dat$date <= input$timeFrame[2],]
+    
+    
+    df <- dat %>% group_by(year,month_num,month,activity) %>% dplyr::summarise(sum_time = sum(time))
+    df <- df[!is.na(df$sum_time),]
+    df$date <- paste(df$year,df$month_num,"01",sep="-")
+    df$date <- as.Date(df$date)
+    
+    ggplot(df, aes(x=date,y=sum_time,fill=activity)) +
+      geom_bar(stat="identity", position="stack") +
+      theme_bw() +
+      scale_fill_manual(values = col24)
+    
+  })
+  
+  output$distance_bar_weekly <- renderPlot({
+    
+    dat <- data_import()
+    dat <- dat[dat$date >= input$timeFrame[1] & dat$date <= input$timeFrame[2],]
+    
+    
+    df <- dat %>% group_by(year,week,activity) %>% dplyr::summarise(sum_distance = sum(distance.km.))
+    df <- df[!is.na(df$sum_distance),]
+    
+    ggplot(df, aes(x=week,y=sum_distance,fill=activity)) +
+      geom_bar(stat="identity", position="stack") +
+      theme_bw() +
+      facet_grid(year~.) +
+      scale_fill_manual(values = col24)
+    
+  })
+  
+  output$minutes_bar_weekly <- renderPlot({
+    
+    dat <- data_import()
+    dat <- dat[dat$date >= input$timeFrame[1] & dat$date <= input$timeFrame[2],]
+    
+    
+    df <- dat %>% group_by(year,week,activity) %>% dplyr::summarise(sum_time = sum(time))
+    df <- df[!is.na(df$sum_time),]
+    
+    ggplot(df, aes(x=week,y=sum_time,fill=activity)) +
+      geom_bar(stat="identity", position="stack") +
+      theme_bw() +
+      facet_grid(year~.) +
+      scale_fill_manual(values = col24)
+    
+  })
+  
+  output$distance_bar_monthly <- renderPlot({
+    
+    dat <- data_import()
+    dat <- dat[dat$date >= input$timeFrame[1] & dat$date <= input$timeFrame[2],]
+    
+    
+    df <- dat %>% group_by(year,month_num,month,activity) %>% dplyr::summarise(sum_distance = sum(distance.km.))
+    df <- df[!is.na(df$sum_distance),]
+    df$date <- paste(df$year,df$month_num,"01",sep="-")
+    df$date <- as.Date(df$date)
+    
+    ggplot(df, aes(x=date,y=sum_distance,fill=activity)) +
+      geom_bar(stat="identity", position="stack") +
+      theme_bw() +
+      scale_fill_manual(values = col24)
+    
+  })
+  
+  
+  #   output$wordcloud <- renderPlot({
+  #     
+  #     dat <- data_import()
+  #     dat <- dat[dat$date >= input$timeFrame[1] & dat$date <= input$timeFrame[2],]
+  #     
+  #     myCorpus <- Corpus(VectorSource(dat[["description"]]))
+  #     myCorpus = tm_map(myCorpus, content_transformer(tolower))
+  #     myCorpus = tm_map(myCorpus, removePunctuation)
+  #     myCorpus = tm_map(myCorpus, removeNumbers)
+  #     myCorpus = tm_map(myCorpus, removeWords,
+  #                       c(stopwords("SMART"), "thy", "thou", "thee", "the", "and", "but"))
+  #   
+  #     myDTM = TermDocumentMatrix(myCorpus,
+  #                                control = list(minWordLength = 1))
+  #     m = as.matrix(myDTM)
+  #     sort(rowSums(m), decreasing = TRUE)
+  #     wordcloud(m, scale=c(4,0.5), 
+  #                   colors=brewer.pal(8, "Dark2"))  
+  #   })
+  
+  
+}
--- ui.R
+++ ui.R
@@ -1,43 +1,99 @@
+library(shinydashboard)
 library(shiny)
-#library(shinyBS)
 library(DT)
 
-shinyUI(fluidPage(
-  title = "Attackpoint explorer",
-  
-#   tags$head(
-#     # Include our custom CSS
-#     #includeCSS("styles.css"),
-#     # Hide the red error messages!!!
-#     tags$style(type="text/css",
-#               ".shiny-output-error { visibility: hidden; }",
-#                ".shiny-output-error:before { visibility: hidden; }"
-#     )
-#   ), 
-  tags$h2("Attackpoint explorer"),
-  tags$a(href="https://github.com/muuankarski/attackpoint_explorer","Source code in Github"),
-  tags$hr(),
-    fluidRow(
-    shiny::column(4, radioButtons("dataType", "Own or test data",inline = FALSE, choices = list("Markus test data","Upload Own"))),
-    shiny::column(4, uiOutput("file_input")),
-    shiny::column(4, radioButtons("paceType", "Show pace/speed",inline = FALSE, choices = list("min/km","km/h")))
-    ),
-  fluidRow(
-    shiny::column(4, uiOutput("activity_list")),
-    shiny::column(4, uiOutput("time_frame")),
-    shiny::column(4, uiOutput("select_cols"))
-  ),
-  tags$hr(),
-  
-  tabsetPanel(tabPanel("Table", DT::dataTableOutput("mytable")),
-              tabPanel("Distance vs. duration Scatterplots", 
-                      plotOutput("scatter",
-                                 height="600px", width="auto")),
-              tabPanel("Monthly mean pace/speed", plotOutput("pace_bar",height="600px", width="auto")),
-              tabPanel("Monthly training minutes per activity", plotOutput("minutes_bar_monthly",height="600px", width="auto")),
-              tabPanel("Monthly training distance per activity", plotOutput("distance_bar_monthly",height="600px", width="auto")),
-              tabPanel("Weekly training minutes per activity", plotOutput("minutes_bar_weekly",height="600px", width="auto")),
-              tabPanel("Weekly training distance per activity", plotOutput("distance_bar_weekly",height="600px", width="auto"))
-)
-)
+dashboardPage(skin = "black",
+              dashboardHeader(title = "Attackpoint explorer"),
+              dashboardSidebar(
+                sidebarMenu(
+                  menuItem("Upload and View data", tabName = "upload", icon = icon("file-text")),
+                  uiOutput("time_frame"),
+                  menuItem("Monthly summaries", tabName = "monthly", icon = icon("bar-chart")),
+                  menuItem("Weekly summaries", tabName = "weekly", icon = icon("bar-chart")),
+                  #menuItem("Sentiment analysis of comments", tabName = "sentiment", icon = icon("user-md")),
+                  menuItem("Source code", icon = icon("github"), 
+                           href = "https://github.com/muuankarski/attackpoint_explorer")
+                )
+                
+                
+              ),
+              dashboardBody(
+                tabItems(
+                  # First tab content
+                  tabItem(tabName = "upload",
+                          fluidRow(
+                            box(title = NULL, width = 4, 
+                                radioButtons("dataType", "" ,inline = FALSE, 
+                                             choices = list("Markus test data","Upload Own")
+                                )
+                            ),
+                            
+                            box(
+                              title = NULL, width=4,
+                              uiOutput("file_input")
+                            ),
+                            box(title=NULL,width=4,
+                                uiOutput("select_cols")
+                            )
+                          ),
+                          fluidRow(
+                            box(title = "Browse the data", width = 12, 
+                                DT::dataTableOutput("mytable")
+                            )
+                          )
+                  ),
+                  
+                  # Second tab content
+                  tabItem(tabName = "monthly",
+                          fluidRow(
+                            box(title=NULL, width=3,
+                                radioButtons("paceTypeMonthly", "Show pace/speed",
+                                             inline = FALSE, 
+                                             choices = list("min/km","km/h"))
+                            ),
+                            box(title=NULL, width=6,
+                                uiOutput("activity_list_monthly")
+                            )
+                          ),
+                          fluidRow(  
+                            tabBox(title=NULL, width=12,
+                                   side = "left", height = "250px",
+                                   tabPanel("Mean pace", plotOutput("pace_point_monthly")),
+                                   tabPanel("Monthly training minutes per activity", plotOutput("minutes_bar_monthly")),
+                                   tabPanel("Monthly training distance per activity", plotOutput("distance_bar_monthly"))
+                            )
+                          )
+                  ),
+                  # Second tab content
+                  tabItem(tabName = "weekly",
+                          fluidRow(
+                            box(title=NULL, width=3,
+                                radioButtons("paceTypeWeekly", "Show pace/speed",
+                                             inline = FALSE, 
+                                             choices = list("min/km","km/h"))),
+                            box(title=NULL, width=6,
+                                uiOutput("activity_list_weekly")
+                            )
+                          ),
+                          fluidRow(  
+                            tabBox(title=NULL, width=12,
+                                   side = "left", height = "250px",
+                                   tabPanel("Mean pace", plotOutput("pace_point_weekly")),
+                                   tabPanel("Weekly training minutes per activity", plotOutput("minutes_bar_weekly")),
+                                   tabPanel("Weekly training distance per activity", plotOutput("distance_bar_weekly"))
+                            )
+                          )
+                          
+                  )#,
+                  #       # Sentiment analysis
+                  #       tabItem(tabName = "sentiment",
+                  #               fluidRow(
+                  #                 box(title=NULL, width=7,
+                  #                     tags$img(src = "http://content.gallup.com/origin/gallupinc/GallupSpaces/Production/Cms/POLL/fuvzeznhr0ar7mtpmyh2xg.jpg", width = "350", height = "350")
+                  #               )
+                  #               )
+                  #               
+                  #       )
+                )
+              )
 )
